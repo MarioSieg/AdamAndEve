4b24d2e438591605e3823d9f706d78cde61ec161
msi <support@kerbogames.com>
2021-05-04 22:40:44 +0200

    DiskImage

--- Build.sh
+++ Build.sh
@@ -1,5 +1,5 @@
 as -o Boot.o Boot16.asm
 ld -o Boot.tmp -Ttext 0x7c00 Boot.o
-objcopy -O binary Boot.tmp -j .text Booster.bin
+objcopy -O binary Boot.tmp -j .text Booster.iso
 rm Boot.o
 rm Boot.tmp
